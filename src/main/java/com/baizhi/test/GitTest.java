b6c7ca160a047bb45ae28385d1ae87f251fc509e
1204424831 <34115031+1204424831@users.noreply.github.com>
2019-11-11 14:35:16 +0800

    Update GitTest.java

--- src/main/java/com/baizhi/test/GitTest.java
+++ src/main/java/com/baizhi/test/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("aaa");
         System.out.println("haha");
+        System.out.printLn("111");
     }
 }
