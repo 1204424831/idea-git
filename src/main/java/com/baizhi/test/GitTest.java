fb84e6297a21cd38c3c76a96a3c5fcf69dcee164
yfz <1204424831@qq.com>
2019-11-11 14:36:01 +0800

    this is v3.0

--- src/main/java/com/baizhi/test/GitTest.java
+++ src/main/java/com/baizhi/test/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("aaa");
         System.out.println("haha");
-        System.out.printLn("111");
+        System.out.println("111");
     }
 }
